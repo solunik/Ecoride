1330eede8750dea2d2208210ce40d5b46976e3b7
Hammoumi <hammoumi.a@hotmail.fr>
2025-03-03 14:23:03 +0100

    mise en place de root via index.php

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -2,3 +2,24 @@
 function accueilPage() {
     require __DIR__ . '/../views/accueil.php';
 }
+
+function connexionPage() {
+    require __DIR__ . '/../views/connexion.php';
+}
+
+function contactPage() {
+    require __DIR__ . '/../views/contact.php';
+}
+
+function covoituragesPage() {
+    require __DIR__ . '/../views/covoiturages.php';
+}
+
+function inscriptionPage() {
+    require __DIR__ . '/../views/inscription.php';
+}
+
+function recherchePage() {
+    require __DIR__ . '/../views/recherche.php';
+}
+?>
